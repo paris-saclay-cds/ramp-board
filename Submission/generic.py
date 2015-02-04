702695e03aeea6a6b1dda9e07df45dc71a090c13
Balazs Kegl <balazs.kegl@gmail.com>
2015-02-04 10:23:28 +0100

    leaderboard_classical comments

--- Submission/generic.py
+++ Submission/generic.py
@@ -27,6 +27,34 @@ def train_model(m_path, X, y, skf):
         print acc
 
 def leaderboard_classical(m_paths):
+    """Output classical leaderboard (sorted in decreasing order by score).
+
+    Parameters
+    ----------
+    m_paths : array-like, shape = [n_models]
+        A list of paths, each containing a "score.csv" file with three columns:
+            1) the file prefix (hash of the test index set the model was tested on)
+            2) the number of test points
+            3) the test score (the lower the better; error)
+    Returns
+    -------
+    leaderboard : a pandas DataFrame with two columns:
+            1) The model name (the name of the subdir)
+            2) the mean score
+        example:
+            model     error
+        0   Kegl9  0.232260
+        1  Kegl10  0.232652
+        2   Kegl7  0.234174
+        3   Kegl1  0.237330
+        4   Kegl2  0.238945
+        5   Kegl4  0.242344
+        6   Kegl5  0.243212
+        7   Kegl6  0.243380
+        8   Kegl8  0.244015
+        9   Kegl3  0.251158
+
+    """
     mean_scores = []
     m_names = []
     for m_path in m_paths:
@@ -36,9 +64,9 @@ def leaderboard_classical(m_paths):
         scores = pd.read_csv(m_path + "/score.csv", names=["h_str", "n", "score"])
         mean_scores = np.append(mean_scores, scores['score'].mean())
         m_names = np.append(m_names, m_name)
-    print mean_scores[0]
+    #print mean_scores[0]
     ordering = mean_scores.argsort() # error: decreasing order
-    print mean_scores[ordering]
+    #print mean_scores[ordering]
     leaderboard = pd.DataFrame()
     leaderboard['model'] = m_names[ordering]
     leaderboard['error'] = mean_scores[ordering]
