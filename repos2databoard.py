b8885c7f011135ae71fde5da4daaca3cfeacdcb2
cds dev <cds@cds.com>
2015-02-07 21:23:05 +0100

    add submission fetching script

---
+++ repos2databoard.py
@@ -0,0 +1,61 @@
+# Author: Alexandre Gramfort <alexandre.gramfort@telecom-paristech.fr>
+# License: BSD 3 clause
+
+import glob
+import os
+import time
+import git
+import numpy as np
+import pandas as pd
+
+base_path = '/mnt/datacamp/leaderboard/TeamsRepos/'
+repo_paths = sorted(glob.glob(os.path.join(base_path, '*')))
+
+submissions_path = "/mnt/datacamp/databoard/Submission/Models"
+# submissions_path = os.path.join('/mnt', 'submissions')
+
+if not os.path.exists(submissions_path):
+    os.mkdir(submissions_path)
+
+tags_info = []
+
+for rp in repo_paths:
+    if not os.path.isdir(rp):
+        continue
+
+    team_name = os.path.basename(rp)
+    repo = git.Repo(rp)
+    o = repo.remotes.origin
+    o.pull()
+    print rp
+
+    if len(repo.tags) > 0:
+        for t in repo.tags:
+            tag_name = t.name
+            c = t.commit
+            tree = repo.tree(c.hexsha)
+            b = tree['model.py']
+            file_content = b.data_stream.read()
+            model_path = os.path.join(submissions_path, team_name)
+            if not os.path.exists(model_path):
+                os.mkdir(model_path)
+            model_path = os.path.join(model_path, tag_name)
+            if not os.path.exists(model_path):
+                os.mkdir(model_path)
+            model_path = os.path.join(model_path, 'model.py')
+            with open(model_path, 'w') as f:
+                f.write(file_content)
+            tags_info.append([team_name, tag_name, c.committed_date, model_path])
+    else:
+        print('No tag found for %s' % team_name)
+
+if len(tags_info) > 0:
+    columns = ['team', 'model', 'timestamp', 'path']
+    df = pd.DataFrame(np.array(tags_info), columns=columns)
+    print df
+
+    print('Writting submission.csv file')
+    df.to_csv('submissions.csv', index=False)
+else:
+    print('No submission found')
+
