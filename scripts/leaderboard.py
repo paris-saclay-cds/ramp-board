516f16263d86867529dd6fcbcc468f179dbbd036
Balazs Kegl <balazs.kegl@gmail.com>
2015-02-08 16:23:23 +0100

    scripts/leaderboard.py added to generate leaderboard at terminal

---
+++ scripts/leaderboard.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+import os
+from databoard.generic import leaderboard_classical, leaderboard_combination, leaderboard_to_html
+import glob
+from config_databoard import root_path
+
+trained_models = pd.read_csv("trained_submissions.csv")
+print trained_models
+
+l1 = leaderboard_classical(trained_models)
+print l1
+#l1_html = leaderboard_to_html(l1)
+#print l1_html
+
+
+#gt_path = os.path.join(root_path, 'Submission', 'GroundTruth')
+#l2 = leaderboard_combination(trained_models, gt_path)
+#print l2
+#l2_html = leaderboard_to_html(l2)
+#print l2_html
