e3cb5e3b1bdf979119480f7da4e0db5c857ae1ae
cds dev <cds@cds.com>
2015-02-09 11:23:29 +0100

    config

--- config_databoard.py
+++ config_databoard.py
@@ -1,8 +1,8 @@
 root_path = "."
 test_size = 0.5
 random_state = 57
-n_processes = 4
-n_CV = n_processes
+n_processes =  12
+n_CV = 5*n_processes
 
 # root_url="http://onevm-217.lal.in2p3.fr:8080/list/"
 # home_url="/mnt/datacamp/leaderboard"
