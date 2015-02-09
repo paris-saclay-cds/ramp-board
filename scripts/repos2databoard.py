d5405211235d1080bab38b111f96eb97045d2030
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2015-02-09 12:13:44 +0100

    add try for fetch

--- scripts/repos2databoard.py
+++ scripts/repos2databoard.py
@@ -30,26 +30,29 @@ for rp in repo_paths:
     o.pull()
     print rp
 
-    if len(repo.tags) > 0:
-        for t in repo.tags:
-            tag_name = t.name
-            c = t.commit
-            tree = repo.tree(c.hexsha)
-            b = tree['model.py']
-            file_content = b.data_stream.read()
-            model_path = os.path.join(submissions_path, team_name)
-            if not os.path.exists(model_path):
-                os.mkdir(model_path)
-            model_path = os.path.join(model_path, tag_name)
-            if not os.path.exists(model_path):
-                os.mkdir(model_path)
-            model_path = os.path.join(model_path, 'model.py')
-            with open(model_path, 'w') as f:
-                f.write(file_content)
-            relative_model_path = os.path.join(team_name, tag_name)
-            tags_info.append([team_name, tag_name, c.committed_date, relative_model_path])
-    else:
-        print('No tag found for %s' % team_name)
+    try:
+        if len(repo.tags) > 0:
+            for t in repo.tags:
+                tag_name = t.name
+                c = t.commit
+                tree = repo.tree(c.hexsha)
+                b = tree['model.py']
+                file_content = b.data_stream.read()
+                model_path = os.path.join(submissions_path, team_name)
+                if not os.path.exists(model_path):
+                    os.mkdir(model_path)
+                model_path = os.path.join(model_path, tag_name)
+                if not os.path.exists(model_path):
+                    os.mkdir(model_path)
+                model_path = os.path.join(model_path, 'model.py')
+                with open(model_path, 'w') as f:
+                    f.write(file_content)
+                relative_model_path = os.path.join(team_name, tag_name)
+                tags_info.append([team_name, tag_name, c.committed_date, relative_model_path])
+        else:
+            print('No tag found for %s' % team_name)
+    except Exception, e:
+        print "Error: %s" % e
 
 if len(tags_info) > 0:
     columns = ['team', 'model', 'timestamp', 'path']
