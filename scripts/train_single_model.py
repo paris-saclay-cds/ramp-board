a62e282385e1b6b87b0888bf2e92ee20ee3cae54
Balazs Kegl <balazs.kegl@gmail.com>
2015-02-21 23:37:29 +0100

    tabs

--- scripts/train_single_model.py
+++ scripts/train_single_model.py
@@ -6,9 +6,9 @@ from generic import train_models
 @click.command()
 @click.option('--alias', default='kegl/rf300')
 def train(alias):
-	models = pd.read_csv("output/submissions.csv")
-	model = models[models['alias'] == alias]
-	train_models(model)
+    models = pd.read_csv("output/submissions.csv")
+    model = models[models['alias'] == alias]
+    train_models(model)
 
 if __name__ == '__main__':
     train()
--- specific.py
+++ specific.py
@@ -4,19 +4,20 @@ from config_databoard import local_deployment
 import pandas as pd
 
 def read_data(filename='input/train.csv'):
-	test_size = 0.5
-	random_state = 57
-	n_CV = 2 if local_deployment else 5 * n_processes
-	df = pd.read_csv(filename)
-	y = df['TARGET'].values
-	X = df.drop('TARGET', axis=1).values
-	skf = StratifiedShuffleSplit(y, n_iter=n_CV, test_size=test_size, random_state=random_state)
-	return X, y, skf
+    test_size = 0.5
+    random_state = 57
+    n_CV = 2 if local_deployment else 5 * n_processes
+    df = pd.read_csv(filename)
+    y = df['TARGET'].values
+    X = df.drop('TARGET', axis=1).values
+    skf = StratifiedShuffleSplit(y, n_iter=n_CV, test_size=test_size, random_state=random_state)
+    return X, y, skf
 
 def run_model(model, X_train, y_train, X_test):
-	clf = model.Classifier()
-	clf.fit(X_train, y_train)
-	y_pred = clf.predict(X_test)
-	y_score = clf.predict_proba(X_test)
-	return y_pred, y_score
+    clf = model.Classifier()
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    y_score = clf.predict_proba(X_test)
+    return y_pred, y_score
+    
 
