8da6cf33de068c83e08ce0d3e09f387e5f379e91
Balazs Kegl <balazs.kegl@gmail.com>
2015-03-29 23:39:32 +0200

    - regrouping scores into a "menu" file  - adding unit tests for scores

---
+++ databoard/scores.py
@@ -0,0 +1,30 @@
+"""All score classes have a score function taking a ground truth vector
+    and a prediction vector and returning a score; and a boolean 
+    higher_the_better."""
+
+# Author: Balazs Kegl
+# License: BSD 3 clause
+
+from sklearn.metrics import accuracy_score, roc_curve, auc
+
+class ScoreAuc():
+    def __init__(self):
+        self.higher_the_better = True
+
+    def score(self, y_test_list, y_pred_list):
+        fpr, tpr, _ = roc_curve(y_test_list, y_pred_list)
+        return auc(fpr, tpr)
+
+class ScoreAccuracy():
+    def __init__(self):
+        self.higher_the_better = True
+
+    def score(self, y_test_list, y_pred_list):
+        return accuracy_score(y_test_list, y_pred_list)
+
+class ScoreError():
+    def __init__(self):
+        self.higher_the_better = False
+
+    def score(self, y_test_list, y_pred_list):
+        return 1 - accuracy_score(y_test_list, y_pred_list)
