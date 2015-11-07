c922b9b7a029039ddc860271803f1e7187768e0b
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2015-11-07 13:46:41 +0100

    cleanup

--- ramp_test.sh
+++
@@ -1,8 +0,0 @@
-# fab publish_test:mortality_prediction_local_test
-# cd /tmp/databoard_local/databoard_mortality_prediction_8080
-# fab test_ramp
-
-fab publish_test:iris_local_test
-cd /tmp/databoard_local/databoard_iris_8080
-fab test_ramp
-
