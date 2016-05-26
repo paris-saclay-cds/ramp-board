cb7d3aacf3af27a0d3f563889dc219c3ca61cf96
camille24 <camille.marini@gmail.com>
2016-05-26 15:50:28 +0200

    added stress tests with locust

--- README.md
+++ README.md
@@ -86,9 +86,11 @@ By default `port=None` (for local profiling) and `profiling_output_file=profiler
 #### Database performance
 To report in the logging system queries that takes too long, define an environment variable `DATABOARD_DB_PERF` (equals to 'True' for instance).   
 #### Stress Tests with [Locust](http://locust.io/)  
-1. Modify `locustfile.py` to specify the current ramp url to be tested (or to add some tasks) and define two environment variables `DATABOARD_USERNAME` and `DATABOARD_PASSWORD` to login during tests.     
-2. Run `locust -f locustfile.py` 
-3. Go to http://127.0.0.1:8089/ and enter the number of users to simulate  
+1. Modify `locustfile.py` to specify the current ramp url to be tested and the databoard path (or to add some tasks)
+2. Define two environment variables `DATABOARD_USERNAME` and `DATABOARD_PASSWORD` to login during tests.     
+3. Set `WTF_CSRF_ENABLED` to `False` in `databoard/config.py`
+4. Run `locust -f locustfile.py` 
+5. Go to http://127.0.0.1:8089/ and enter the number of users to simulate  
 
 
 ### Example sequence of adding the drug_spectra ramp
--- locustfile.py
+++ locustfile.py
@@ -5,9 +5,13 @@ import numpy as np
 from locust import HttpLocust
 from locust import TaskSet
 from locust import task
+import databoard.config as config
 
 
 event_name = 'iris_test'  # name of the event to be tested
+databoard_path = '/tmp/databoard_test'  # path where databoard is published
+
+assert not config.WTF_CSRF_ENABLED, 'Set WTF_CSRF_ENABLED to False'
 
 
 class DataboardTasks(TaskSet):
@@ -21,14 +25,14 @@ class DataboardTasks(TaskSet):
         self.login()
 
     def login(self):
-        response = self.client.get("/login")
-        csrf_token = response.cookies["session"]
+        # response = self.client.get("/login")
+        # csrf_token = response.cookies["session"]
         user_name = os.environ.get("DATABOARD_USERNAME")
         user_pwd = os.environ.get("DATABOARD_PASSWORD")
         self.client.post("/login", {"user_name": user_name,
-                                    "password": user_pwd,
-                                    "csrf_token": csrf_token},
-                         headers={"X-CSRFToken": csrf_token})
+                                    "password": user_pwd})  # ,
+        #                            "csrf_token": csrf_token},
+        #                 headers={"X-CSRFToken": csrf_token})
 
     @task(10)
     def leaderboard(self):
@@ -39,9 +43,12 @@ class DataboardTasks(TaskSet):
         submission_id = np.random.randint(1000000)
         submission_name = "locust_%i" % submission_id
         print(submission_name)
-        # self.client.post("/events/" + event_name + "/sandbox",
-        #                  {'submission_name': submission_name})
-        self.client.get("/events/" + event_name + "/sandbox")
+        self.client.post("/events/" + event_name + "/sandbox",
+                         {'submission_name': submission_name})
+        os.system('cd %s; fab train_test:%s,t=%s,s=%s'
+                  % (databoard_path, event_name,
+                     os.environ.get("DATABOARD_USERNAME"),
+                     submission_name))
 
 
 class WebsiteUser(HttpLocust):
