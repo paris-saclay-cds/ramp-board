a19790776560e14adee7d38bacf838962611d140
camille24 <camille.marini@gmail.com>
2016-06-13 13:49:42 +0200

    backup files

---
+++ tools/dump_db.sh
@@ -0,0 +1,15 @@
+#!/bin/sh    
+# Backup databoard database and databoard files
+databoard_path=/home/databoard
+backup_path=/mnt/datacamp/backup
+db=databoard
+
+# dump db
+date=`date +"%Y%m%d_%H%M%N"`
+filename="${backup_path}/${db}_${date}.tar"
+pg_dump -U postgres -Ft $db > $filename
+
+# rsync databoard files
+rsync -a ${databoard_path}/ $backup_path/databoard
+
+exit 0
