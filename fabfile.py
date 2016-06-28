f0de9065ab276f275504053625afd44b69269482
camille24 <camille.marini@gmail.com>
2016-06-28 11:12:23 +0200

    added line to run tests several times (clean test database)

--- fabfile.py
+++ fabfile.py
@@ -30,7 +30,7 @@ def test_setup():
         os.mkdir(config.db_path)
     open(os.path.join(config.submissions_path, '__init__.py'), 'a').close()
 
-    #recreate_test_db()
+    recreate_test_db()
     db_tools.setup_score_types()
     db_tools.setup_workflows()
     db_tools.add_problem('iris')
