5a7dd614fca8b6b6e54fead976b63fbae935847b
camille24 <camille.marini@gmail.com>
2016-06-29 10:45:40 +0200

    script to add event official_score_name for existing events

---
+++ tools/add_event_official_score_name.py
@@ -0,0 +1,18 @@
+# 29/06/16
+# Script to add in the table Event official_score_name after backuping the
+# old sqlite database
+from databoard import db
+from databoard.model import Event
+
+score_name = {'drug_spectra': 'combined',
+              'air_passengers_dssp4': 'rmse',
+              'boston_housing_test': 'rmse',
+              'epidemium2_cancer_mortality': 'rmse',
+              'iris_test': 'accuracy',
+              'HEP_detector_anomalies': 'auc'}
+
+for kk, vv in score_name.items():
+    event = Event.query.filter_by(name=kk).one()
+    event.official_score_name = vv
+    db.session.add(event)
+    db.session.commit()
