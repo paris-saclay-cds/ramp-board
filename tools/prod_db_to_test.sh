c620fa808904a00865fc206c8922cebc14c09444
camille24 <camille.marini@gmail.com>
2016-12-22 15:48:31 +0100

    added doc

--- tools/prod_db_to_test.sh
+++ tools/prod_db_to_test.sh
@@ -6,6 +6,15 @@
 # - migration files are up-to-date on test server
 # on the test server (in backup_path)
 # The current test database is removed and replace by a copy of the prod database
+# Needs the following env variables:
+#    export DATABOARD_DB_NAME='databoard'
+#    export DATABOARD_DB_USER='<prod_db_user>'     # Ask Balazs
+#    export DATABOARD_DB_PASSWORD='<prod_db_pwd>'  # Ask Balazs
+#    export SCIENCEFS_LOGIN='balazs.kegl'          # You need the private key or the password...
+#    export mount_path='mount_backup'              # path where to mount the scienceFS disk to get backups
+#    export DATABOARD_PATH='/tmp'                  #root path to your databoard app. By default, on your local computer it is /tmp, so that the app is in /tmp/datacamp/databoard. For prod and test servers it is '/mnt/ramp_data'.                     
+#    export prod_db_dump='<blabla.dump>'           # db dump to be used (just write the dump name without the path to it
+
 
 # mount sciencefs disk
 sshfs -o Ciphers=arcfour256 -o allow_other -o IdentityFile=${SCIENCEFS_ID} -o StrictHostKeyChecking=no "$SCIENCEFS_LOGIN"@sciencefs.di.u-psud.fr:/sciencefs/homes/"$SCIENCEFS_LOGIN"/databoard ${mount_path}
