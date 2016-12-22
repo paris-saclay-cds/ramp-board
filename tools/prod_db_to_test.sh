ee4f38f1f0b3e4903c7dcadb456ca4d2a3d003a6
camille24 <camille.marini@gmail.com>
2016-12-22 12:40:01 +0100

    fix wrong path

--- tools/prod_db_to_test.sh
+++ tools/prod_db_to_test.sh
@@ -7,9 +7,6 @@
 # on the test server (in backup_path)
 # The current test database is removed and replace by a copy of the prod database
 
-# backup directory of the prod server
-backup_path=/mnt/datacamp/backup
-
 # mount sciencefs disk
 sshfs -o Ciphers=arcfour256 -o allow_other -o IdentityFile=/root/.ssh/id_rsa_sciencefs -o StrictHostKeyChecking=no "$SCIENCEFS_LOGIN"@sciencefs.di.u-psud.fr:/sciencefs/homes/"$SCIENCEFS_LOGIN"/databoard ${mount_path}
 
@@ -20,9 +17,9 @@ psql -U postgres -c '\i tools/setup_database.sql'
 # Pb with db user password..not properly set with the above script... workaround:
 psql -U postgres -c "ALTER ROLE $DATABOARD_DB_USER WITH PASSWORD '$DATABOARD_DB_PASSWORD'"
 # Restore the db
-pg_restore -j 8 -U postgres -d ${DATABOARD_DB_NAME} ${backup_path}/${prod_db_dump}
+pg_restore -j 8 -U postgres -d ${DATABOARD_DB_NAME} ${mount_path}/backup/${prod_db_dump}
 # rsync databoard files
-rsync -a $backup_path/databoard ${DATABOARD_PATH}/datacamp/databoard
+rsync -a ${mount_path}/backup/databoard ${DATABOARD_PATH}/datacamp/databoard
 
 # unmount sciencefs disk
 fusermount -u ${mount_path}
