1ed2b16ad2c814fc854ef8ce6131f61b73241134
camille24 <camille.marini@gmail.com>
2016-12-22 16:16:30 +0100

    fix bug test init

--- databoard/__init__.py
+++ databoard/__init__.py
@@ -14,7 +14,7 @@ __version__ = '0.1.dev'
 
 app = Flask('databoard')
 app.config.from_object('databoard.config.Config')
-if os.environ.get('DATABOARD_TEST'):
+if bool(os.environ.get('DATABOARD_TEST')):
     app.config.from_object('databoard.config.TestingConfig')
 app.debug = False
 # app.debug_mode = True
