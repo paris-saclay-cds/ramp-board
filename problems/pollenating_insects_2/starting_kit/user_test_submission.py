83a0b12b15e80582a17a0c580c8d2614044fc15e
Balazs Kegl <balazs.kegl@gmail.com>
2017-05-25 11:37:17 +0200

    new score in user test

--- problems/pollenating_insects_2/starting_kit/user_test_submission.py
+++ problems/pollenating_insects_2/starting_kit/user_test_submission.py
@@ -1,17 +1,12 @@
 # coding=utf-8
-import time
-import os
-import re
-import glob
 import logging
 
 import numpy as np
 import pandas as pd
-from skimage.io import imread
 
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.metrics import accuracy_score
-from sklearn.metrics import make_scorer
+from sklearn.metrics import f1_score
 
 from batch_classifier_workflow import train_submission
 from batch_classifier_workflow import test_submission
@@ -25,6 +20,7 @@ attrs = {
     'n_classes': 209
 }
 
+
 def read_data(filename):
     df = pd.read_csv(filename)
     X_values = df['id'].values
@@ -32,10 +28,19 @@ def read_data(filename):
     y = df['class'].values
     return X, y
 
+
 def get_cv(y_train_array):
     return StratifiedShuffleSplit(n_splits=2, test_size=0.5, random_state=43)
 
 score_function = accuracy_score
+
+
+def score_function_2(y_true, y_pred):
+    """Rate of classes with f1 score above 0.5."""
+    f1 = f1_score(y_true, y_pred, average=None)
+    score = 1. * len(f1[f1 > 0.5]) / len(f1)
+    return score
+
 
 if __name__ == '__main__':
     # PIL image loading logger is a bit annoying, so disable it.
@@ -48,5 +53,9 @@ if __name__ == '__main__':
         print("Testing ...")
         y_pred = test_submission(trained_model, X[test_is])
         score = score_function(y[test_is], y_pred.argmax(axis=1))
-        np.savez('pred_{}.npz'.format(i), y_true=y[test_is], y_pred=y_pred.argmax(axis=1))
+        np.savez(
+            'pred_{}.npz'.format(i), y_true=y[test_is],
+            y_pred=y_pred.argmax(axis=1))
         print('accuracy = ', score)
+        score_2 = score_function_2(y[test_is], y_pred.argmax(axis=1))
+        print('rate of classes with f1 score > 0.5 = ', score_2)
