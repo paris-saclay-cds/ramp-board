aefff851627b46c3650ad6370b4d51399f63168a
Balazs Kegl <balazs.kegl@gmail.com>
2017-08-27 18:09:42 +0200

    remove old torch and parallelism code

--- interface/rampinterface/torch/model.lua
+++
@@ -1,5 +0,0 @@
-function model(X_train, y_train, X_test)
-    proba = torch.rand(X_test:size(1), 2)
-    targets = argmax_2D(proba) - 1
-    return targets, proba
-end
--- interface/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="rampinterface",
-    version="0.1",
-    packages=["rampinterface"]
-)
