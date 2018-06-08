b282c23ca33159da1f8f9622400bfb6b43db3ae4
Balazs Kegl <balazs.kegl@gmail.com>
2018-06-08 14:04:29 +0200

    Checking if deployment dir exists (#47)

--- fabfile.py
+++ fabfile.py
@@ -44,7 +44,8 @@ def deploy_locally():
 
     os.system('rm -rf ' + config.local_test_deployment_path)
     os.makedirs(config.local_test_deployment_path)
-    os.makedirs(config.deployment_path)
+    if not os.path.isdir(config.deployment_path):
+        os.makedirs(config.deployment_path)
     os.system('rsync -rultv fabfile.py ' + config.deployment_path)
     os.makedirs(config.ramp_kits_path)
     os.makedirs(config.ramp_data_path)
