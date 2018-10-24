139cda413c5ea7924bc4ea20cf252e3d86e95eea
Balazs Kegl <balazs.kegl@gmail.com>
2018-10-24 11:33:09 +0200

    test add users

--- fabfile.py
+++ fabfile.py
@@ -8,44 +8,6 @@ from termcolor import colored
 logger = logging.getLogger('databoard')
 
 
-def add_test_users():
-    import databoard.db_tools as db_tools
-
-    db_tools.create_user(
-        name='kegl', password='pwd',
-        lastname='Kegl', firstname='Balazs',
-        email='balazs.kegl@gmail.com', access_level='admin')
-    db_tools.create_user(
-        name='agramfort', password='pwd',
-        lastname='Gramfort', firstname='Alexandre',
-        email='alexandre.gramfort@gmail.com', access_level='admin')
-    db_tools.create_user(
-        name='akazakci', password='pwd',
-        lastname='Akin', firstname='Kazakci',
-        email='osmanakin@gmail.com', access_level='admin')
-    db_tools.create_user(
-        name='mcherti', password='pwd', lastname='Cherti',
-        firstname='Mehdi', email='mehdicherti@gmail.com',
-        access_level='admin')
-    db_tools.create_user(
-        name='test_user', password='test',
-        lastname='Test', firstname='User',
-        email='test.user@gmail.com', access_level='user')
-    db_tools.create_user(
-        name='test_iris_admin', password='test',
-        lastname='Admin', firstname='Iris',
-        email='iris.admin@gmail.com', access_level='user')
-
-
-def deploy_locally():
-    from databoard.deploy import deploy
-    import databoard.db_tools as db_tools
-
-    deploy()
-    add_test_users()
-    db_tools.setup_workflows()
-
-
 def test_keywords():
     import databoard.db_tools as db_tools
     db_tools.add_keyword('botany', 'data_domain', 'scientific data', 'Botany.')
