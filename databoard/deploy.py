726e6a2aec72a44534b04e912bbd9d8286202fd0
Alexandre Boucaud <boucaud.alexandre@gmail.com>
2018-10-25 17:33:03 +0200

    Update deployment

--- databoard/deploy.py
+++ databoard/deploy.py
@@ -1,6 +1,7 @@
 import os
 from databoard import db
-import databoard.config as config
+from databoard import (deployment_path, ramp_config, 
+                       ramp_kits_path, ramp_data_path)
 
 
 def recreate_db():
@@ -12,13 +13,13 @@ def recreate_db():
 
 
 def deploy():
-    if os.getenv('DATABOARD_TEST'):
-        os.system('rm -rf ' + config.local_test_deployment_path)
-        os.makedirs(config.local_test_deployment_path)
-        if not os.path.isdir(config.deployment_path):
-            os.makedirs(config.deployment_path)
-        os.system('rsync -rultv fabfile.py ' + config.deployment_path)
-        os.makedirs(config.ramp_kits_path)
-        os.makedirs(config.ramp_data_path)
-        os.makedirs(config.submissions_path)
+    if os.getenv('DATABOARD_STAGE') in ['TEST', 'TESTING']:
+        os.unlink(deployment_path)
+        os.makedirs(deployment_path)
+        os.system('rsync -rultv fabfile.py {}'.format(deployment_path))
+        os.makedirs(ramp_kits_path)
+        os.makedirs(ramp_data_path)
+        os.makedirs(
+            os.path.join(deployment_path, ramp_config['submissions_dir'])
+        )
         recreate_db()
