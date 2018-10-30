13196bc5c01418299b304c521652c3a6b43b6b64
Alexandre Boucaud <boucaud.alexandre@gmail.com>
2018-10-30 11:45:51 +0100

    Remove unused import

--- databoard/__init__.py
+++ databoard/__init__.py
@@ -5,7 +5,6 @@ from flask import Flask
 from flask_mail import Mail
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-from distutils.util import strtobool
 
 __version__ = '0.1.dev'
 
