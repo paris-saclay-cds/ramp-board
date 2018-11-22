287a3f6f33ad3473975dc23f20edda327a716a2f
Alexandre Boucaud <boucaud.alexandre@gmail.com>
2018-11-22 17:32:54 +0100

    Add missing references

--- ramp-database/rampdb/tools.py
+++ ramp-database/rampdb/tools.py
@@ -1,6 +1,9 @@
+import numpy as np
+
 from .model import Team
 from .model import Event
 from .model import EventTeam
+from .model import combine_predictions_list
 
 __all__ = [
     'get_active_user_event_team',
@@ -9,6 +12,7 @@ __all__ = [
     'get_team_members',
     'get_user_teams',
     'get_user_event_teams',
+    'get_next_best_single_fold',
 ]
 
 
@@ -46,3 +50,64 @@ def get_user_event_teams(event_name, user_name):
 
 def get_n_user_teams(user):
     return len(get_user_teams(user))
+
+
+def get_next_best_single_fold(event, predictions_list, ground_truths,
+                              best_index_list):
+    """.
+
+    Find the model that minimizes the score if added to
+    predictions_list[best_index_list] using event.official_score_function.
+    If there is no model improving the input
+    combination, the input best_index_list is returned. Otherwise the best
+    model is added to the list. We could also return the combined prediction
+    (for efficiency, so the combination would not have to be done each time;
+    right now the algo is quadratic), but I don't think any meaningful
+    rule will be associative, in which case we should redo the combination from
+    scratch each time the set changes. Since now combination = mean, we could
+    maintain the sum and the number of models, but it would be a bit bulky.
+    We'll see how this evolves.
+
+    Parameters
+    ----------
+    predictions_list : list of instances of Predictions
+        Each element of the list is an instance of Predictions of a model
+        on the same (cross-validation valid) data points.
+    ground_truths : instance of Predictions
+        The ground truth.
+    best_index_list : list of integers
+        Indices of the current best model.
+
+    Returns
+    -------
+    best_index_list : list of integers
+        Indices of the models in the new combination. If the same as input,
+        no models wer found improving the score.
+    """
+    best_predictions = combine_predictions_list(
+        predictions_list, index_list=best_index_list)
+    best_score = event.official_score_function(
+        ground_truths, best_predictions)
+    best_index = -1
+    # Combination with replacement, what Caruana suggests. Basically, if a
+    # model is added several times, it's upweighted, leading to
+    # integer-weighted ensembles
+    r = np.arange(len(predictions_list))
+    # Randomization doesn't matter, only in case of exact equality.
+    # np.random.shuffle(r)
+    # print r
+    for i in r:
+        combined_predictions = combine_predictions_list(
+            predictions_list, index_list=np.append(best_index_list, i))
+        new_score = event.official_score_function(
+            ground_truths, combined_predictions)
+        is_lower_the_better = event.official_score_type.is_lower_the_better
+        if (is_lower_the_better and new_score < best_score) or\
+                (not is_lower_the_better and new_score > best_score):
+            best_predictions = combined_predictions
+            best_index = i
+            best_score = new_score
+    if best_index > -1:
+        return np.append(best_index_list, best_index), best_score
+    else:
+        return best_index_list, best_score
