15f8c3453c55ad65b81031225d9ef27c0790c6fe
Alexandre Boucaud <boucaud.alexandre@gmail.com>
2018-11-22 13:40:41 +0100

    add independent database model as ramp-database

---
+++ ramp-database/rampdb/model/datatype.py
@@ -0,0 +1,19 @@
+import zlib
+import numpy as np
+
+from sqlalchemy import LargeBinary
+from sqlalchemy import TypeDecorator
+
+__all__ = ['NumpyType']
+
+
+class NumpyType(TypeDecorator):
+    """Storing zipped numpy arrays."""
+    impl = LargeBinary
+
+    def process_bind_param(self, value, dialect):
+        # we convert the initial value into np.array to handle None and lists
+        return zlib.compress(np.array(value).dumps())
+
+    def process_result_value(self, value, dialect):
+        return np.loads(zlib.decompress(value))
