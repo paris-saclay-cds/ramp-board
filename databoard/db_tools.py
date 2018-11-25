e09944c5551b9c73220d9eb1f7000ee4bcc75221
Balazs Kegl <balazs.kegl@gmail.com>
2018-11-25 18:01:02 +0100

    getting commits out of model

--- databoard/db_tools.py
+++ databoard/db_tools.py
@@ -25,9 +25,7 @@ from .model import (CVFold, DetachedSubmissionOnCVFold,
                     SubmissionFileTypeExtension, SubmissionOnCVFold,
                     SubmissionSimilarity, Team, TooEarlySubmissionError, User,
                     UserInteraction, Workflow, WorkflowElement,
-                    WorkflowElementType, _get_score_cv_bags,
-                    combine_predictions_list,
-                    get_next_best_single_fold)
+                    WorkflowElementType)
 from .utils import (date_time_format, get_hashed_password, remove_non_ascii,
                     send_mail, table_format)
 
@@ -92,8 +90,212 @@ def get_active_user_event_team(event, user):
     # on members but it is slow, eg in constructing user interactions
     # event_teams = EventTeam.query.filter_by(event=event).all()
     # for event_team in event_teams:
-    #     if user in get_team_members(event_team.team) and event_team.is_active:
+    #     if user in get_team_members(event_team.team) and
+    #             event_team.is_active:
     #         return event_team
+
+
+def combine_predictions_list(predictions_list, index_list=None):
+    """Combine predictions in predictions_list[index_list].
+
+    By taking the mean of their get_combineable_predictions views.
+
+    E.g. for regression it is the actual
+    predictions, and for classification it is the probability array (which
+    should be calibrated if we want the best performance). Called both for
+    combining one submission on cv folds (a single model that is trained on
+    different folds) and several models on a single fold.
+    Called by
+    _get_bagging_score : which combines bags of the same model, trained on
+        different folds, on the heldout test set
+    _get_cv_bagging_score : which combines cv-bags of the same model, trained
+        on different folds, on the training set
+    get_next_best_single_fold : which does one step of the greedy forward
+        selection (of different models) on a single fold
+    _get_combined_predictions_single_fold : which does the full loop of greedy
+        forward selection (of different models), until improvement, on a single
+        fold
+    _get_combined_test_predictions_single_fold : which computes the combination
+        (constructed on the cv valid set) on the holdout test set, on a single
+        fold
+    _get_combined_test_predictions : which combines the foldwise combined
+        and foldwise best test predictions into a single megacombination
+
+    Parameters
+    ----------
+    predictions_list : list of instances of Predictions
+        Each element of the list is an instance of Predictions of a given model
+        on the same data points.
+    index_list : None | list of integers
+        The subset of predictions to be combined. If None, the full set is
+        combined.
+
+    Returns
+    -------
+    combined_predictions : instance of Predictions
+        A predictions instance containing the combined (averaged) predictions.
+    """
+    Predictions = type(predictions_list[0])
+    combined_predictions = Predictions.combine(predictions_list, index_list)
+    return combined_predictions
+
+
+def _get_score_cv_bags(event, score_type, predictions_list, ground_truths,
+                       test_is_list=None):
+    """
+    Compute the bagged score of the predictions in predictions_list.
+
+    Called by Submission.compute_valid_score_cv_bag and
+    db_tools.compute_contributivity.
+
+    Parameters
+    ----------
+    event : instance of Event
+        Needed for the type of y_comb and
+    predictions_list : list of instances of Predictions
+    ground_truths : instance of Predictions
+    test_is_list : list of integers
+        Indices of points that should be bagged in each prediction. If None,
+        the full prediction vectors will be bagged.
+    Returns
+    -------
+    score_cv_bags : instance of Score ()
+    """
+    if test_is_list is None:  # we combine the full list
+        test_is_list = [range(len(predictions.y_pred))
+                        for predictions in predictions_list]
+
+    y_comb = np.array(
+        [event.Predictions(n_samples=len(ground_truths.y_pred))
+         for _ in predictions_list])
+    score_cv_bags = []
+    for i, test_is in enumerate(test_is_list):
+        y_comb[i].set_valid_in_train(predictions_list[i], test_is)
+        combined_predictions = combine_predictions_list(y_comb[:i + 1])
+        valid_indexes = combined_predictions.valid_indexes
+        score_cv_bags.append(score_type.score_function(
+            ground_truths, combined_predictions, valid_indexes))
+        # XXX maybe use masked arrays rather than passing valid_indexes
+    return combined_predictions, score_cv_bags
+
+
+def get_next_best_single_fold(event, predictions_list, ground_truths,
+                              best_index_list, min_improvement=0.0):
+    """.
+
+    Find the model that minimizes the score if added to
+    predictions_list[best_index_list] using event.official_score_function.
+    If there is no model improving the input
+    combination, the input best_index_list is returned. Otherwise the best
+    model is added to the list. We could also return the combined prediction
+    (for efficiency, so the combination would not have to be done each time;
+    right now the algo is quadratic), but I don't think any meaningful
+    rule will be associative, in which case we should redo the combination from
+    scratch each time the set changes. Since now combination = mean, we could
+    maintain the sum and the number of models, but it would be a bit bulky.
+    We'll see how this evolves.
+
+    Parameters
+    ----------
+    predictions_list : list of instances of Predictions
+        Each element of the list is an instance of Predictions of a model
+        on the same (cross-validation valid) data points.
+    ground_truths : instance of Predictions
+        The ground truth.
+    best_index_list : list of integers
+        Indices of the current best model.
+
+    Returns
+    -------
+    best_index_list : list of integers
+        Indices of the models in the new combination. If the same as input,
+        no models wer found improving the score.
+    """
+    best_predictions = combine_predictions_list(
+        predictions_list, index_list=best_index_list)
+    best_score = event.official_score_function(
+        ground_truths, best_predictions)
+    best_index = -1
+    # Combination with replacement, what Caruana suggests. Basically, if a
+    # model is added several times, it's upweighted, leading to
+    # integer-weighted ensembles
+    r = np.arange(len(predictions_list))
+    # Randomization doesn't matter, only in case of exact equality.
+    # np.random.shuffle(r)
+    # print r
+    for i in r:
+        combined_predictions = combine_predictions_list(
+            predictions_list, index_list=np.append(best_index_list, i))
+        new_score = event.official_score_function(
+            ground_truths, combined_predictions)
+        is_lower_the_better = event.official_score_type.is_lower_the_better
+        if (is_lower_the_better and new_score < best_score) or\
+                (not is_lower_the_better and new_score > best_score):
+            best_predictions = combined_predictions
+            best_index = i
+            best_score = new_score
+    if best_index > -1:
+        return np.append(best_index_list, best_index), best_score
+    else:
+        return best_index_list, best_score
+
+
+def compute_valid_score_cv_bag(submission):
+    """Cv-bag cv_fold.valid_predictions using combine_predictions_list.
+
+    The predictions in predictions_list[i] belong to those indicated
+    by self.on_cv_folds[i].test_is.
+    """
+    ground_truths_train = submission.event.problem.ground_truths_train()
+    if submission.state == 'tested':
+        predictions_list = [submission_on_cv_fold.valid_predictions for
+                            submission_on_cv_fold in submission.on_cv_folds]
+        test_is_list = [submission_on_cv_fold.cv_fold.test_is for
+                        submission_on_cv_fold in submission.on_cv_folds]
+        for score in submission.scores:
+            _, score.valid_score_cv_bags = _get_score_cv_bags(
+                submission.event, score.event_score_type, predictions_list,
+                ground_truths_train, test_is_list)
+            score.valid_score_cv_bag = float(score.valid_score_cv_bags[-1])
+    else:
+        for score in submission.scores:
+            score.valid_score_cv_bag = float(score.event_score_type.worst)
+            score.valid_score_cv_bags = None
+    db.session.commit()
+
+
+def compute_test_score_cv_bag(submission):
+    """Bag cv_fold.test_predictions using combine_predictions_list.
+
+    And stores the score of the bagged predictor in test_score_cv_bag. The
+    scores of partial combinations are stored in test_score_cv_bags.
+    This is for assessing the bagging learning curve, which is useful for
+    setting the number of cv folds to its optimal value (in case the RAMP
+    is competitive, say, to win a Kaggle challenge; although it's kinda
+    stupid since in those RAMPs we don't have a test file, so the learning
+    curves should be assessed in compute_valid_score_cv_bag on the
+    (cross-)validation sets).
+    """
+    if submission.state == 'tested':
+        # When we have submission id in Predictions, we should get the
+        # team and submission from the db
+        ground_truths = submission.event.problem.ground_truths_test()
+        predictions_list = [submission_on_cv_fold.test_predictions for
+                            submission_on_cv_fold in submission.on_cv_folds]
+        combined_predictions_list = [
+            combine_predictions_list(predictions_list[:i + 1]) for
+            i in range(len(predictions_list))]
+        for score in submission.scores:
+            score.test_score_cv_bags = [
+                score.score_function(
+                    ground_truths, combined_predictions) for
+                combined_predictions in combined_predictions_list]
+            score.test_score_cv_bag = float(score.test_score_cv_bags[-1])
+    else:
+        for score in submission.scores:
+            score.test_score_cv_bag = float(score.event_score_type.worst)
+            score.test_score_cv_bags = None
+    db.session.commit()
 
 
 def send_password_mail(user_name, password):
@@ -1054,8 +1256,8 @@ def score_submission(submission):
             submission_on_cv_fold.compute_test_scores()
             submission_on_cv_fold.state = 'scored'
         db.session.commit()
-        submission.compute_test_score_cv_bag()
-        submission.compute_valid_score_cv_bag()
+        compute_test_score_cv_bag(submission)
+        compute_valid_score_cv_bag(submission)
         # Means and stds were constructed on demand by fetching fold times.
         # It was slow because submission_on_folds contain also possibly large
         # predictions. If postgres solves this issue (which can be tested on
