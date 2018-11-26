e461aec136a81307f4811a3521d82dd85e105378
Alexandre Boucaud <boucaud.alexandre@gmail.com>
2018-11-26 14:53:59 +0100

    Update travis

--- ci_tools/travis/test_ramp_board.sh
+++ ci_tools/travis/test_ramp_board.sh
@@ -1,9 +1,13 @@
-if [[ "$PYTHON_VERSION" == 2.7 ]]; then
-    psql -U postgres -c "CREATE USER mrramp WITH PASSWORD 'mrramp';ALTER USER mrramp WITH SUPERUSER;"
-    createdb --owner=mrramp databoard_test
-    export DATABOARD_TEST=True
-    export DATABOARD_DB_URL=postgresql://mrramp:mrramp@localhost/databoard_test
-    export DATABOARD_DB_URL_TEST=postgresql://mrramp:mrramp@localhost/databoard_test
-    cp databoard/config_local.py databoard/config.py
-    make test
-fi
+export DATABOARD_TEST=True
+# export DATABOARD_DB_URL=postgresql://mrramp:mrramp@localhost/databoard_test
+export DATABOARD_DB_URL_TEST=postgresql://mrramp:mrramp@localhost/databoard_test
+
+# mkdir postgres_dbs
+# initdb postgres_dbs
+# pg_ctl -D postgres_dbs -l postgres_dbs/logfile start
+psql -U postgres -c "CREATE USER mrramp WITH PASSWORD 'mrramp';ALTER USER mrramp WITH SUPERUSER;"
+createdb --owner=mrramp databoard_test
+# create a user and set a password
+# createuser --pwprompt mrramp
+# create the database
+pytest -vsl databoard
