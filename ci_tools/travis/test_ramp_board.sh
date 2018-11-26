cf21f159395f0a8d7ae629a8266c26e56e0f88bf
Alexandre Boucaud <boucaud.alexandre@gmail.com>
2018-11-26 15:56:47 +0100

    Refactor databoard travis tests

--- ci_tools/travis/test_ramp_board.sh
+++ ci_tools/travis/test_ramp_board.sh
@@ -1,13 +1,4 @@
-export DATABOARD_TEST=True
-# export DATABOARD_DB_URL=postgresql://mrramp:mrramp@localhost/databoard_test
-export DATABOARD_DB_URL_TEST=postgresql://mrramp:mrramp@localhost/databoard_test
+psql -U postgres -c "CREATE USER ${DATABOARD_USER} WITH PASSWORD '${DATABOARD_PASSWORD}';ALTER USER ${DATABOARD_USER} WITH SUPERUSER;"
+createdb --owner=${DATABOARD_USER} databoard_test
 
-# mkdir postgres_dbs
-# initdb postgres_dbs
-# pg_ctl -D postgres_dbs -l postgres_dbs/logfile start
-psql -U postgres -c "CREATE USER mrramp WITH PASSWORD 'mrramp';ALTER USER mrramp WITH SUPERUSER;"
-createdb --owner=mrramp databoard_test
-# create a user and set a password
-# createuser --pwprompt mrramp
-# create the database
 pytest -vsl databoard
