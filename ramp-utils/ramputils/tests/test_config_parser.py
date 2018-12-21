ca9eb68cec61179ea020d8b0fc4cece7227c3994
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2018-12-21 13:30:09 +0100

    TST: fix test for configuration

--- ramp-utils/ramputils/tests/test_config_parser.py
+++ ramp-utils/ramputils/tests/test_config_parser.py
@@ -5,4 +5,14 @@ from ramputils import read_config
 def test_read_config():
     config_file = path_config_example()
     config = read_config(config_file)
-    print(config)
+    expected_config = {
+        'sqlalchemy': {
+            'drivername': 'postgresql',
+            'username': 'mrramp',
+            'password': 'mrramp',
+            'host': 'localhost',
+            'port': 5432,
+            'database': 'databoard_test'
+        }
+    }
+    assert config == expected_config
