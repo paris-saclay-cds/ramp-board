4e2ac8f13710713110fed6a33d4b99bf972b268c
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2018-12-23 16:52:19 +0100

    EHN: add common utilities

--- ramp-utils/ramputils/__init__.py
+++ ramp-utils/ramputils/__init__.py
@@ -1,9 +1,14 @@
 from .config_parser import read_config
 
+from .string_encoding import encode_string
+
 from .utils import import_module_from_source
 
 from .worker import generate_worker_config
 
-__all__ = ['generate_worker_config'
-           'import_module_from_source',
-           'read_config']
+__all__ = [
+    'encode_string',
+    'generate_worker_config',
+    'import_module_from_source',
+    'read_config'
+]
