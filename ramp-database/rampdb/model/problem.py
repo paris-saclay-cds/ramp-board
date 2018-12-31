8f8cb91131c3d7b9b4d3be71dc6f72793b54f1f4
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2018-12-31 15:56:23 +0100

    TST/DOC: Problem

--- ramp-database/rampdb/model/problem.py
+++ ramp-database/rampdb/model/problem.py
@@ -25,15 +25,45 @@ __all__ = [
 
 
 class Problem(Model):
+    """Problem table.
+
+    Parameters
+    ----------
+    name : str
+        The name of the problem.
+    path_ramp_kits : str
+        The path where the kits are located. It will corresponds to
+        the key `ramp_kits_dir` of the dictionary created with
+        :func:`ramputils.generate_ramp_config`.
+    path_ramp_data : str
+        The path where the data are located. It will corresponds to
+        the key `ramp_data_dir` of the dictionary created with
+        :func:`ramputils.generate_ramp_config`.
+    session : :class:`sqlalchemy.orm.Session`
+        The session to directly perform the operation on the database.
+
+    Attributes
+    ----------
+    id : int
+        The ID of the table row.
+    name : str
+        The name of the problem.
+    workflow_id : id
+        The ID of the associated workflow.
+    workflow : :class:`rampdb.model.Worflow`
+        The workflow instance.
+    path_ramp_kits : str
+        The path where the kits are located.
+    path_ramp_data : str
+        The path where the data are located.
+    """
     __tablename__ = 'problems'
 
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False, unique=True)
 
-    workflow_id = Column(
-        Integer, ForeignKey('workflows.id'), nullable=False)
-    workflow = relationship(
-        'Workflow', backref=backref('problems'))
+    workflow_id = Column(Integer, ForeignKey('workflows.id'), nullable=False)
+    workflow = relationship('Workflow', backref=backref('problems'))
 
     # XXX: big change in the database
     path_ramp_kits = Column(String, nullable=False, unique=False)
@@ -44,21 +74,19 @@ class Problem(Model):
         self.path_ramp_kits = path_ramp_kits
         self.path_ramp_data = path_ramp_data
         self.reset(session)
-        # to check if the module and all required fields are there
-        self.module
-        self.Predictions
-        self.workflow_object
 
     def __repr__(self):
         return 'Problem({})\n{}'.format(encode_string(self.name),
                                         self.workflow)
 
     def reset(self, session):
+        """Reset the workflow."""
         if session is not None:
             self.workflow = \
                 (session.query(Workflow)
-                        .filter(Workflow.name == type(self.module.workflow)
-                        .__name__).one())
+                        .filter(Workflow.name ==
+                                type(self.module.workflow).__name__)
+                        .one())
         else:
             self.workflow = \
                 (Workflow.query
@@ -67,6 +95,7 @@ class Problem(Model):
 
     @property
     def module(self):
+        """module: Get the problem module."""
         return import_module_from_source(
             os.path.join(self.path_ramp_kits, self.name, 'problem.py'),
             'problem'
@@ -74,34 +103,44 @@ class Problem(Model):
 
     @property
     def title(self):
+        """str: The title of the problem."""
         return self.module.problem_title
 
     @property
     def Predictions(self):
+        """:class:`rampwf.prediction_types`: The predictions used for the
+        problem."""
         return self.module.Predictions
 
     def get_train_data(self):
+        """ndarray: The training data."""
         path = os.path.join(self.path_ramp_data, self.name)
         return self.module.get_train_data(path=path)
 
     def get_test_data(self):
+        """ndarray: The testing data."""
         path = os.path.join(self.path_ramp_data, self.name)
         return self.module.get_test_data(path=path)
 
     def ground_truths_train(self):
+        """ndarray: The true labels for the training."""
         _, y_train = self.get_train_data()
         return self.Predictions(y_true=y_train)
 
     def ground_truths_test(self):
+        """ndarray: the true labels for the testing."""
         _, y_test = self.get_test_data()
         return self.Predictions(y_true=y_test)
 
     def ground_truths_valid(self, test_is):
+        """ndarray: the true labels for the validation."""
         _, y_train = self.get_train_data()
         return self.Predictions(y_true=y_train[test_is])
 
     @property
     def workflow_object(self):
+        """:class:`rampwf.worflows`: The workflow instance used for the
+        problem."""
         return self.module.workflow
 
 
