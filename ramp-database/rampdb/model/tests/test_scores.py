6bd5743a103e86b7d4ce74592b133ca49f512918
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2018-12-31 16:25:18 +0100

    DOC/TST: ScoreType

---
+++ ramp-database/rampdb/model/tests/test_scores.py
@@ -0,0 +1,39 @@
+import shutil
+
+import pytest
+
+from ramputils import read_config
+from ramputils.testing import path_config_example
+
+
+from rampdb.model import Model
+from rampdb.model import ScoreType
+
+from rampdb.utils import setup_db
+from rampdb.utils import session_scope
+from rampdb.testing import create_toy_db
+
+
+@pytest.fixture(scope='module')
+def config():
+    return read_config(path_config_example())
+
+
+@pytest.fixture(scope='module')
+def session_scope_module(config):
+    try:
+        create_toy_db(config)
+        with session_scope(config['sqlalchemy']) as session:
+            yield session
+    finally:
+        shutil.rmtree(config['ramp']['deployment_dir'], ignore_errors=True)
+        db, Session = setup_db(config['sqlalchemy'])
+        with db.connect() as conn:
+            session = Session(bind=conn)
+            session.close()
+        Model.metadata.drop_all(db)
+
+
+def test_score_type_model(session_scope_module):
+    score_type = session_scope_module.query(ScoreType).first()
+    assert 'ScoreType(name=' in repr(score_type)
