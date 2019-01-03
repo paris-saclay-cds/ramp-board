125341a149d35bd33c5f82cb1120a8509647d402
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2019-01-03 15:01:48 +0100

    iter

--- ramp-database/rampdb/model/tests/test_user.py
+++ ramp-database/rampdb/model/tests/test_user.py
@@ -12,7 +12,7 @@ from rampdb.utils import setup_db
 from rampdb.utils import session_scope
 from rampdb.testing import create_toy_db
 
-from rampdb.tools.user import get_team_by_name
+from rampdb.tools.user import get_user_by_name
 
 
 @pytest.fixture(scope='module')
@@ -35,8 +35,11 @@ def session_scope_module(config):
         Model.metadata.drop_all(db)
 
 
-def test_team_model(session_scope_module):
-    team = get_team_by_name(session_scope_module, 'test_user')
-    assert re.match(r'Team\(name=.*test_user.*, admin_name=.*test_user.*\)',
-                    repr(team))
-    assert re.match(r'Team\(.*test_user.*\)', str(team))
+def test_user_model_properties(session_scope_module):
+    user = get_user_by_name(session_scope_module, 'test_user')
+
+    assert user.is_active is True
+    assert user.is_anonymous is False
+    assert user.get_id() == '1'
+    assert re.match(r'User\(.*test_user.*\)', str(user))
+    assert re.match(r'User\(name=.*test_user.*, lastname.*\)', repr(user))
