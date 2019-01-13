3366d74e1e174d4289a3c4cf60981998710e44bb
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2019-01-13 01:13:50 +0100

    DOC: add documentation to the form

--- ramp-frontend/frontend/tasks.py
+++
@@ -1,9 +0,0 @@
-from invoke import task
-
-
-@task
-def launch(c):
-    from application import create_app
-    app = create_app()
-    app.run(debug=False, port=8080, use_reloader=False,
-            host='127.0.0.1', processes=1000, threaded=False)
