ed5d2197a2f7509368d50791d01005e8ed9510e1
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2019-01-14 14:43:55 +0100

    TST: add test database model (#118)

---
+++ ramp-database/rampdb/model/tests/test_problem.py
@@ -0,0 +1,86 @@
+import shutil
+
+import pytest
+
+from ramputils.utils import encode_string
+from ramputils import read_config
+from ramputils.testing import path_config_example
+
+from rampwf.prediction_types.base import BasePrediction
+from rampwf.workflows.classifier import Classifier
+
+from rampdb.model import Event
+from rampdb.model import Model
+from rampdb.model import ProblemKeyword
+
+from rampdb.utils import setup_db
+from rampdb.utils import session_scope
+from rampdb.testing import create_toy_db
+
+from rampdb.tools.event import get_problem
+
+
+@pytest.fixture(scope='module')
+def config():
+    return read_config(path_config_example())
+
+
+@pytest.fixture(scope='module')
+def session_scope_module(config):
+    try:
+        create_toy_db(config)
+        with session_scope(config['sqlalchemy']) as session:
+            yield session
+    finally:
+        shutil.rmtree(config['ramp']['deployment_dir'], ignore_errors=True)
+        db, Session = setup_db(config['sqlalchemy'])
+        with db.connect() as conn:
+            session = Session(bind=conn)
+            session.close()
+        Model.metadata.drop_all(db)
+
+
+def test_problem_model(session_scope_module):
+    problem = get_problem(session_scope_module, 'iris')
+
+    assert (repr(problem) ==
+            "Problem({})\nWorkflow(Classifier)\n\tWorkflow(Classifier): "
+            "WorkflowElement(classifier)".format(encode_string('iris')))
+
+    # check that we can access the problem module and that we have one of the
+    # expected function there.
+    assert hasattr(problem.module, 'get_train_data')
+
+    assert problem.title == 'Iris classification'
+    assert issubclass(problem.Predictions, BasePrediction)
+    X_train, y_train = problem.get_train_data()
+    assert X_train.shape == (120, 4)
+    assert y_train.shape == (120,)
+    X_test, y_test = problem.get_test_data()
+    assert X_test.shape == (30, 4)
+    assert y_test.shape == (30,)
+    gt_train = problem.ground_truths_train()
+    assert hasattr(gt_train, 'label_names')
+    assert gt_train.y_pred.shape == (120, 3)
+    gt_test = problem.ground_truths_test()
+    assert hasattr(gt_test, 'label_names')
+    assert gt_test.y_pred.shape == (30, 3)
+    gt_valid = problem.ground_truths_valid([0, 1, 2])
+    assert hasattr(gt_valid, 'label_names')
+    assert gt_valid.y_pred.shape == (3, 3)
+
+    assert isinstance(problem.workflow_object, Classifier)
+
+
+@pytest.mark.parametrize(
+    'backref, expected_type',
+    [('events', Event),
+     ('keywords', ProblemKeyword)]
+)
+def test_problem_model_backref(session_scope_module, backref, expected_type):
+    problem = get_problem(session_scope_module, 'iris')
+    backref_attr = getattr(problem, backref)
+    assert isinstance(backref_attr, list)
+    # only check if the list is not empty
+    if backref_attr:
+        assert isinstance(backref_attr[0], expected_type)
---
+++ ramp-database/rampdb/model/tests/test_user.py
@@ -0,0 +1,63 @@
+import re
+import shutil
+
+import pytest
+
+from ramputils import read_config
+from ramputils.testing import path_config_example
+
+from rampdb.model import EventAdmin
+from rampdb.model import Model
+from rampdb.model import SubmissionSimilarity
+from rampdb.model import Team
+
+from rampdb.utils import setup_db
+from rampdb.utils import session_scope
+from rampdb.testing import create_toy_db
+
+from rampdb.tools.user import get_user_by_name
+
+
+@pytest.fixture(scope='module')
+def config():
+    return read_config(path_config_example())
+
+
+@pytest.fixture(scope='module')
+def session_scope_module(config):
+    try:
+        create_toy_db(config)
+        with session_scope(config['sqlalchemy']) as session:
+            yield session
+    finally:
+        shutil.rmtree(config['ramp']['deployment_dir'], ignore_errors=True)
+        db, Session = setup_db(config['sqlalchemy'])
+        with db.connect() as conn:
+            session = Session(bind=conn)
+            session.close()
+        Model.metadata.drop_all(db)
+
+
+def test_user_model_properties(session_scope_module):
+    user = get_user_by_name(session_scope_module, 'test_user')
+
+    assert user.is_active is True
+    assert user.is_anonymous is False
+    assert user.get_id() == '1'
+    assert re.match(r'User\(.*test_user.*\)', str(user))
+    assert re.match(r'User\(name=.*test_user.*, lastname.*\)', repr(user))
+
+
+@pytest.mark.parametrize(
+    'backref, expected_type',
+    [('admined_events', EventAdmin),
+     ('submission_similaritys', SubmissionSimilarity),
+     ('admined_teams', Team)]
+)
+def test_user_model_backref(session_scope_module, backref, expected_type):
+    user = get_user_by_name(session_scope_module, 'test_user')
+    backref_attr = getattr(user, backref)
+    assert isinstance(backref_attr, list)
+    # only check if the list is not empty
+    if backref_attr:
+        assert isinstance(backref_attr[0], expected_type)
