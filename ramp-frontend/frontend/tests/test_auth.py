ae559ccfc9b18d3990c9dc4f8b92a3c9f69c9bd5
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2019-01-14 11:30:19 +0100

    iter

---
+++ ramp-frontend/frontend/tests/test_auth.py
@@ -0,0 +1,50 @@
+import shutil
+
+import pytest
+
+from ramputils import generate_flask_config
+from ramputils import read_config
+from ramputils.testing import path_config_example
+
+from rampdb.model import Model
+from rampdb.testing import create_toy_db
+from rampdb.utils import setup_db
+from rampdb.utils import session_scope
+
+from frontend import create_app
+from frontend.testing import login
+from frontend.testing import logout
+
+
+@pytest.fixture(scope='module')
+def database_config():
+    return read_config(path_config_example(), filter_section='sqlalchemy')
+
+
+@pytest.fixture(scope='module')
+def config():
+    return read_config(path_config_example())
+
+
+@pytest.fixture(scope='module')
+def client_session(config):
+    try:
+        create_toy_db(config)
+        flask_config = generate_flask_config(config)
+        app = create_app(flask_config)
+        with session_scope(config['sqlalchemy']) as session:
+            yield app.test_client(), session
+    finally:
+        shutil.rmtree(config['ramp']['deployment_dir'], ignore_errors=True)
+        db, Session = setup_db(config['sqlalchemy'])
+        with db.connect() as conn:
+            session = Session(bind=conn)
+            session.close()
+        Model.metadata.drop_all(db)
+
+
+def test_login(client_session):
+    client, session = client_session
+
+    # GET without any previous login
+    rv = client.get('/login')
