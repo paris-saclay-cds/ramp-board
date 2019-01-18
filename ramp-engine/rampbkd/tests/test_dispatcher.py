2bc5bd8fc5349e585ba96807b52bad2fc84cd280
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2019-01-18 23:58:06 +0100

    TST: add test for the dispatcher

--- ramp-engine/rampbkd/tests/test_dispatcher.py
+++ ramp-engine/rampbkd/tests/test_dispatcher.py
@@ -1,11 +1,5 @@
 import shutil
-import sys
-
-PYTHON_MAJOR_VERSION = sys.version_info[0]
-if PYTHON_MAJOR_VERSION >= 3:
-    import tempfile
-else:
-    from backports import tempfile
+import time
 
 import pytest
 
@@ -16,7 +10,10 @@ from rampdb.model import Model
 from rampdb.utils import setup_db
 from rampdb.utils import session_scope
 from rampdb.testing import create_toy_db
+
+from rampdb.tools.event import get_event
 from rampdb.tools.submission import get_submissions
+from rampdb.tools.submission import get_submission_by_id
 
 from rampbkd.local import CondaEnvWorker
 from rampbkd.dispatcher import Dispatcher
@@ -27,8 +24,8 @@ def config():
     return read_config(path_config_example())
 
 
-@pytest.fixture(scope='module')
-def session_scope_module(config):
+@pytest.fixture
+def session_toy(config):
     try:
         create_toy_db(config)
         with session_scope(config['sqlalchemy']) as session:
@@ -39,15 +36,57 @@ def session_scope_module(config):
         Model.metadata.drop_all(db)
 
 
-def test_dispatcher(session_scope_module, caplog, config):
-    # with tempfile.TemporaryDirectory() as local_tmp_dir:
+def test_integration_dispatcher(session_toy, config):
     dispatcher = Dispatcher(config=config,
                             worker=CondaEnvWorker, n_worker=-1,
                             hunger_policy='exit')
     dispatcher.launch()
 
     # the iris kit contain a submission which should fail for each user
-    with session_scope(config['sqlalchemy']) as session:
-        submission = get_submissions(session, config['ramp']['event_name'],
-                                     'training_error')
-        assert len(submission) == 2
+    submission = get_submissions(session_toy, config['ramp']['event_name'],
+                                 'training_error')
+    assert len(submission) == 2
+
+
+def test_unit_test_dispatcher(session_toy, config):
+    # make sure that the size of the list is bigger than the number of
+    # submissions
+    dispatcher = Dispatcher(config=config,
+                            worker=CondaEnvWorker, n_worker=100,
+                            hunger_policy='exit')
+
+    # check that all the queue are empty
+    assert dispatcher._awaiting_worker_queue.empty()
+    assert dispatcher._processing_worker_queue.empty()
+    assert dispatcher._processed_submission_queue.empty()
+
+    # check that all submissions are queued
+    submissions = get_submissions(session_toy, 'iris_test', 'new')
+    dispatcher.fetch_from_db(session_toy)
+    # we should remove the starting kit from the length of the submissions for
+    # each user
+    assert dispatcher._awaiting_worker_queue.qsize() == len(submissions) - 2
+    submissions = get_submissions(session_toy, 'iris_test', 'sent_to_training')
+    assert len(submissions) == 6
+
+    # start the training
+    dispatcher.launch_workers(session_toy)
+    # be sure that the training is finished
+    while not dispatcher._processing_worker_queue.empty():
+        dispatcher.collect_result(session_toy)
+
+    assert len(get_submissions(session_toy, 'iris_test', 'new')) == 2
+    assert (len(get_submissions(session_toy, 'iris_test', 'training_error')) ==
+            2)
+    assert len(get_submissions(session_toy, 'iris_test', 'tested')) == 4
+
+    dispatcher.update_database_results(session_toy)
+    assert dispatcher._processed_submission_queue.empty()
+    event = get_event(session_toy, 'iris_test')
+    assert event.private_leaderboard_html
+    assert event.public_leaderboard_html_with_links
+    assert event.public_leaderboard_html_no_links
+    assert event.failed_leaderboard_html
+    assert event.new_leaderboard_html is None
+    assert event.public_competition_leaderboard_html
+    assert event.private_competition_leaderboard_html
