05e0883e0466602543eb578aa89310b45974204f
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2019-01-19 11:15:17 +0100

    EHN: add tests for the dispatcher command line

--- ramp-engine/rampbkd/cli.py
+++ ramp-engine/rampbkd/cli.py
@@ -10,6 +10,7 @@ from rampbkd.dispatcher import CondaEnvWorker
 
 @click.group()
 def main():
+    """Command-line to launch engine to process RAMP submission."""
     pass
 
 
@@ -18,21 +19,25 @@ def main():
               help='Configuration file in YAML format')
 @click.option('--worker', default='CondaEnvWorker',
               help='Type of worker to use')
-@click.option('--n_worker', default=-1,
+@click.option('--n-worker', default=-1,
               help='Number of worker to start in parallel')
-@click.option('--hunger_policy', default='exit',
+@click.option('--hunger-policy', default='exit',
               help='Policy to apply in case that there is no anymore workers'
               'to be processed')
 @click.option('-v', '--verbose', is_flag=True)
 def dispatcher(config, worker, n_worker, hunger_policy, verbose):
+    """Launch the RAMP dispatcher.
+
+    The RAMP dispatcher is in charge of starting RAMP workers, collecting
+    results from them, and update the database.
+    """
     if verbose:
         logging.basicConfig(format='%(levelname)s %(name)s %(message)s',
                             level=logging.DEBUG)
     config = read_config(config)
-    dispatcher = Dispatcher(config=config,
-                            worker=eval(worker), n_worker=n_worker,
-                            hunger_policy=hunger_policy)
-    dispatcher.launch()
+    disp = Dispatcher(config=config, worker=globals()[worker],
+                      n_worker=n_worker, hunger_policy=hunger_policy)
+    disp.launch()
 
 
 def start():
