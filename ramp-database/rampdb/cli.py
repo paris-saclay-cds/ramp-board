13caba532d46cef54affa306a3313ee8c41c3425
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2019-01-19 11:16:37 +0100

    EHN: Add command-line to interact with the database (#122)

--- ramp-database/rampdb/cli.py
+++ ramp-database/rampdb/cli.py
@@ -1,16 +1,22 @@
-import click
+from collections import defaultdict
 
-from ramputils import generate_ramp_config
+import click
+import pandas as pd
+
 from ramputils import read_config
 
 from .utils import session_scope
 
-from .tools import team
-from .tools import user
+from .tools import event as event_module
+from .tools import leaderboard as leaderboard_module
+from .tools import submission as submission_module
+from .tools import team as team_module
+from .tools import user as user_module
 
 
 @click.group()
 def main():
+    """Command-line to interact directly with the database."""
     pass
 
 
@@ -22,16 +28,17 @@ def main():
 @click.option('--lastname', help="User's last name")
 @click.option('--firstname', help="User's first name")
 @click.option('--email', help="User's email")
-@click.option('--access_level', default='user',
+@click.option('--access-level', default='user',
               help="User's administration rights")
-@click.option('--hidden_notes', default='',
+@click.option('--hidden-notes', default='',
               help="User's additional hidden notes")
 def add_user(config, login, password, lastname, firstname, email,
              access_level, hidden_notes):
+    """Add a new user in the database."""
     config = read_config(config)
     with session_scope(config['sqlalchemy']) as session:
-        user.add_user(session, login, password, lastname, firstname, email,
-                      access_level, hidden_notes)
+        user_module.add_user(session, login, password, lastname, firstname,
+                             email, access_level, hidden_notes)
 
 
 @main.command()
@@ -39,20 +46,158 @@ def add_user(config, login, password, lastname, firstname, email,
               help='Configuration file in YAML format')
 @click.option('--login', help="User's login to be approved")
 def approve_user(config, login):
+    """Approve a user which asked to sign-up to RAMP studio."""
     config = read_config(config)
     with session_scope(config['sqlalchemy']) as session:
-        user.approve_user(session, login)
+        user_module.approve_user(session, login)
 
 
 @main.command()
 @click.option("--config", default='config.yml',
               help='Configuration file in YAML format')
-@click.option('--name', help='Name of the team to sign up')
-def sign_up_team(config, name):
+@click.option('--event', help='Name of the event')
+@click.option('--team', help='Name of the team')
+def sign_up_team(config, event, team):
+    """Sign-up a user (or team) for a RAMP event."""
     config = read_config(config)
-    ramp_config = generate_ramp_config(config)
     with session_scope(config['sqlalchemy']) as session:
-        team.sign_up_team(session, ramp_config['event_name'], name)
+        team_module.sign_up_team(session, event, team)
+
+
+@main.command()
+@click.option("--config", default='config.yml',
+              help='Configuration file in YAML format')
+@click.option('--problem', help='Name of the problem')
+@click.option('--kits-dir', help='Path to the RAMP kits')
+@click.option('--data-dir', help='Path to the RAMP data')
+@click.option('--force', default=False,
+              help='Whether or not to overwrite the problem if it exists')
+def add_problem(config, problem, kits_dir, data_dir, force):
+    """Add a RAMP problem in the database."""
+    config = read_config(config)
+    with session_scope(config['sqlalchemy']) as session:
+        event_module.add_problem(session, problem, kits_dir, data_dir, force)
+
+
+@main.command()
+@click.option("--config", default='config.yml',
+              help='Configuration file in YAML format')
+@click.option("--problem", help='Name of the problem')
+@click.option("--event", help='Name of the event')
+@click.option("--title", help='Title of the event')
+@click.option("--sandbox", default='starting-kit', help='Name of the sandbox')
+@click.option('--submissions-dir',
+              help='Path to the deployment RAMP submissions path.')
+@click.option('--is-public', default=False,
+              help='Whether or not the event should be public')
+@click.option('--force', default=False,
+              help='Whether or not to overwrite the problem if it exists')
+def add_event(config, problem, event, title, sandbox, submissions_dir,
+              is_public, force):
+    """Add an event in the database."""
+    config = read_config(config)
+    with session_scope(config['sqlalchemy']) as session:
+        event_module.add_event(session, problem, event, title, sandbox,
+                               submissions_dir, is_public, force)
+
+
+@main.command()
+@click.option("--config", default='config.yml',
+              help='Configuration file in YAML format')
+@click.option("--event", help='Name of the event')
+@click.option("--user", help='Name of the user becoming event admin')
+def add_event_admin(config, event, user):
+    """Make a user admin of a specific RAMP event."""
+    config = read_config(config)
+    with session_scope(config['sqlalchemy']) as session:
+        event_module.add_event_admin(session, event, user)
+
+
+@main.command()
+@click.option("--config", default='config.yml',
+              help='Configuration file in YAML format')
+@click.option("--event", help='Name of the event')
+@click.option("--team", help='Name of the team')
+@click.option("--submission", help='Name of the submission')
+@click.option("--path", help='Path to the submission')
+def add_submission(config, event, team, submission, path):
+    """Add a submission to the database."""
+    config = read_config(config)
+    with session_scope(config['sqlalchemy']) as session:
+        submission_module.add_submission(session, event, team, submission,
+                                         path)
+
+
+@main.command()
+@click.option("--config", default='config.yml',
+              help='Configuration file in YAML format')
+@click.option("--event", help='Name of the event')
+@click.option("--state", help='The state of the submissions to display')
+def get_submissions_by_state(config, event, state):
+    """Display the list of submission for an event in a particular state."""
+    config = read_config(config)
+    with session_scope(config['sqlalchemy']) as session:
+        submissions = submission_module.get_submissions(session, event, state)
+        if submissions:
+            data = defaultdict(list)
+            for sub_info in submissions:
+                sub = submission_module.get_submission_by_id(
+                    session, sub_info[0]
+                )
+                data['ID'].append(sub.id)
+                data['name'].append(sub.name)
+                data['team'].append(sub.team)
+                data['path'].append(sub.path)
+                data['state'].append(sub.state)
+            click.echo(pd.DataFrame(data).set_index('ID'))
+        else:
+            click.echo('No submission for this event and this state')
+
+
+@main.command()
+@click.option("--config", default='config.yml',
+              help='Configuration file in YAML format')
+@click.option("--submission-id", help='The submission ID')
+@click.option("--state", help='The state to affect to the submission')
+def set_submission_state(config, submission_id, state):
+    """Set the state of a submission."""
+    config = read_config(config)
+    with session_scope(config['sqlalchemy']) as session:
+        submission_module.set_submission_state(session, submission_id, state)
+
+
+@main.command()
+@click.option("--config", default='config.yml',
+              help='Configuration file in YAML format')
+@click.option("--event", help='The event name')
+def update_leaderboards(config, event):
+    """Update the leaderboards for a given event."""
+    config = read_config(config)
+    with session_scope(config['sqlalchemy']) as session:
+        leaderboard_module.update_leaderboards(session, event)
+
+
+@main.command()
+@click.option("--config", default='config.yml',
+              help='Configuration file in YAML format')
+@click.option("--event", help='The event name')
+@click.option("--user", help='The user name')
+def update_user_leaderboards(config, event, user):
+    """Update the user leaderboards for a given event."""
+    config = read_config(config)
+    with session_scope(config['sqlalchemy']) as session:
+        leaderboard_module.update_user_leaderboards(session, event, user)
+
+
+@main.command()
+@click.option("--config", default='config.yml',
+              help='Configuration file in YAML format')
+@click.option("--event", help='The event name')
+def update_all_users_leaderboards(config, event):
+    """Update the leaderboards of all users for a given event."""
+    config = read_config(config)
+    with session_scope(config['sqlalchemy']) as session:
+        leaderboard_module.update_all_user_leaderboards(session, event)
 
 
 def start():
--- ramp-database/rampdb/tests/test_cli.py
+++ ramp-database/rampdb/tests/test_cli.py
@@ -1,25 +1,22 @@
-
+import os
 import shutil
-import subprocess
 
 from click.testing import CliRunner
 
 from ramputils import read_config
+from ramputils import generate_ramp_config
 from ramputils.testing import path_config_example
 
 from rampdb.utils import setup_db
 from rampdb.model import Model
-from rampdb.testing import create_test_db
+from rampdb.testing import create_toy_db
 
 from rampdb.cli import main
 
 
 def setup_module(module):
     config = read_config(path_config_example())
-    create_test_db(config)
-    subprocess.check_call(["ramp-utils",
-                           "deploy-ramp-event",
-                           "--config", path_config_example()])
+    create_toy_db(config)
 
 
 def teardown_module(module):
@@ -38,22 +35,143 @@ def test_add_user():
                                   '--lastname', 'xxx',
                                   '--firstname', 'xxx',
                                   '--email', 'xxx',
-                                  '--access_level', 'admin'],
+                                  '--access-level', 'admin'],
                            catch_exceptions=False)
-    assert result.exit_code == 0
+    assert result.exit_code == 0, result.output
 
 
 def test_approve_user():
     runner = CliRunner()
     result = runner.invoke(main, ['approve-user',
                                   '--config', path_config_example(),
-                                  '--login', 'glemaitre'])
-    assert result.exit_code == 0
+                                  '--login', 'glemaitre'],
+                           catch_exceptions=False)
+    assert result.exit_code == 0, result.output
+
+
+def test_add_problem():
+    runner = CliRunner()
+    ramp_config = generate_ramp_config(path_config_example())
+    result = runner.invoke(main, ['add-problem',
+                                  '--config', path_config_example(),
+                                  '--problem', 'iris',
+                                  '--kits-dir', ramp_config['ramp_kits_dir'],
+                                  '--data-dir', ramp_config['ramp_data_dir'],
+                                  '--force', True],
+                           catch_exceptions=False)
+    assert result.exit_code == 0, result.output
+
+
+def test_add_event():
+    runner = CliRunner()
+    ramp_config = generate_ramp_config(path_config_example())
+    result = runner.invoke(main, ['add-event',
+                                  '--config', path_config_example(),
+                                  '--problem', 'iris',
+                                  '--event', 'iris_test',
+                                  '--title', 'Iris classification',
+                                  '--sandbox', ramp_config['sandbox_name'],
+                                  '--submissions-dir',
+                                  ramp_config['ramp_submissions_dir'],
+                                  '--is-public', False,
+                                  '--force', True],
+                           catch_exceptions=False)
+    assert result.exit_code == 0, result.output
 
 
 def test_sign_up_team():
     runner = CliRunner()
     result = runner.invoke(main, ['sign-up-team',
                                   '--config', path_config_example(),
-                                  '--name', 'glemaitre'])
-    assert result.exit_code == 0
+                                  '--event', 'iris_test',
+                                  '--team', 'glemaitre'],
+                           catch_exceptions=False)
+    assert result.exit_code == 0, result.output
+
+
+
+def test_add_event_admin():
+    runner = CliRunner()
+    result = runner.invoke(main, ['add-event-admin',
+                                  '--config', path_config_example(),
+                                  '--event', 'iris_test',
+                                  '--user', 'glemaitre'],
+                           catch_exceptions=False)
+    assert result.exit_code == 0, result.output
+
+
+def test_add_submission():
+    ramp_config = generate_ramp_config(path_config_example())
+    submission_name = 'new_submission'
+    submission_path = os.path.join(ramp_config['ramp_kit_submissions_dir'],
+                                   submission_name)
+    shutil.copytree(
+        os.path.join(ramp_config['ramp_kit_submissions_dir'],
+                     'random_forest_10_10'),
+        submission_path
+    )
+    runner = CliRunner()
+    result = runner.invoke(main, ['add-submission',
+                                  '--config', path_config_example(),
+                                  '--event', 'iris_test',
+                                  '--team', 'glemaitre',
+                                  '--submission', submission_name,
+                                  '--path', submission_path],
+                           catch_exceptions=False)
+    assert result.exit_code == 0, result.output
+
+
+def test_get_submission_by_state():
+    runner = CliRunner()
+    result = runner.invoke(main, ['get-submissions-by-state',
+                                  '--config', path_config_example(),
+                                  '--event', 'boston_housing_test',
+                                  '--state', 'new'],
+                           catch_exceptions=False)
+    assert result.exit_code == 0, result.output
+    assert "ID" in result.output
+    result = runner.invoke(main, ['get-submissions-by-state',
+                                  '--config', path_config_example(),
+                                  '--event', 'iris_test',
+                                  '--state', 'scored'],
+                           catch_exceptions=False)
+    assert result.exit_code == 0, result.output
+    assert 'No submission for this event and this state' in result.output
+
+
+def test_set_submission_state():
+    runner = CliRunner()
+    result = runner.invoke(main, ['set-submission-state',
+                                  '--config', path_config_example(),
+                                  '--submission-id', 3,
+                                  '--state', 'scored'],
+                           catch_exceptions=False)
+    assert result.exit_code == 0, result.output
+
+
+def test_update_leaderboards():
+    runner = CliRunner()
+    result = runner.invoke(main, ['update-leaderboards',
+                                  '--config', path_config_example(),
+                                  '--event', 'iris_test'],
+                           catch_exceptions=False)
+    assert result.exit_code == 0, result.output
+
+
+def test_update_user_leaderboards():
+    runner = CliRunner()
+    result = runner.invoke(main, ['update-user-leaderboards',
+                                  '--config', path_config_example(),
+                                  '--event', 'iris_test',
+                                  '--user', 'glemaitre'],
+                           catch_exceptions=False)
+    assert result.exit_code == 0, result.output
+
+
+def test_update_all_user_leaderboards():
+    runner = CliRunner()
+    result = runner.invoke(main, ['update-all-users-leaderboards',
+                                  '--config', path_config_example(),
+                                  '--event', 'iris_test'],
+                           catch_exceptions=False)
+    assert result.exit_code == 0, result.output
