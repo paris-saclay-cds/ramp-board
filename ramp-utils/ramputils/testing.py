c3ae9546bca77936e90fb3c996aaddd3aa5aa569
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2019-01-24 00:14:22 +0100

    EHN: split the single configuration into 3 independent config files

--- ramp-utils/ramputils/testing.py
+++ ramp-utils/ramputils/testing.py
@@ -1,7 +1,41 @@
 import os
 
+HERE = os.path.dirname(__file__)
+
 
 def path_config_example():
     """Give the path a ``config.yml`` which can be used as an example."""
-    module_path = os.path.dirname(__file__)
-    return os.path.join(module_path, 'tests', 'data', 'config.yml')
+    return os.path.join(HERE, 'tests', 'data', 'config.yml')
+
+
+def flask_config_template():
+    """Return the path a template Flask configuration file.
+
+    Returns
+    -------
+    filename : str
+        The Flask configuration filename.
+    """
+    return os.path.join(HERE, 'template', 'flask_config.yml')
+
+
+def database_config_template():
+    """Return the path a template database configuration file.
+
+    Returns
+    -------
+    filename : str
+        The database configuration filename.
+    """
+    return os.path.join(HERE, 'template', 'database_config.yml')
+
+
+def ramp_config_template():
+    """Return the path a template RAMP configuration file.
+
+    Returns
+    -------
+    filename : str
+        The RAMP configuration filename.
+    """
+    return os.path.join(HERE, 'template', 'ramp_config.yml')
--- ramp-utils/ramputils/tests/test_testing.py
+++ ramp-utils/ramputils/tests/test_testing.py
@@ -1,8 +1,23 @@
-import os
+from os.path import join
 
+import pytest
+
+from ramputils import read_config
+
+from ramputils.testing import database_config_template
+from ramputils.testing import flask_config_template
 from ramputils.testing import path_config_example
+from ramputils.testing import ramp_config_template
 
 
-def test_path_config_example():
-    path = path_config_example()
-    assert os.path.join('tests', 'data', 'config.yml') in path
+@pytest.mark.parametrize(
+    "config_func, partial_path",
+    [(path_config_example, join('tests', 'data', 'config.yml')),
+     (database_config_template, join('template', 'database_config.yml')),
+     (flask_config_template, join('template', 'flask_config.yml')),
+     (ramp_config_template, join('template', 'ramp_config.yml'))]
+)
+def test_path_configuration(config_func, partial_path):
+    path = config_func()
+    assert partial_path in path
+    read_config(path)
