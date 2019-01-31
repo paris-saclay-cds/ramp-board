128b71050f80c5c224ea9aae0ddb20f4b5aecdf5
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2019-01-31 15:56:42 +0100

    FIX: fix test utils

--- ramp-database/rampdb/tests/test_utils.py
+++ ramp-database/rampdb/tests/test_utils.py
@@ -3,7 +3,8 @@ import shutil
 import pytest
 
 from ramputils import read_config
-from ramputils.testing import path_config_example
+from ramputils.testing import database_config_template
+from ramputils.testing import ramp_config_template
 
 from rampdb.testing import create_test_db
 
@@ -13,29 +14,25 @@ from rampdb.model import SubmissionFileType
 from rampdb.utils import setup_db
 from rampdb.utils import session_scope
 
-
-@pytest.fixture(scope='module')
-def database_config():
-    return read_config(path_config_example(), filter_section='sqlalchemy')
-
-
-@pytest.fixture(scope='module')
-def config():
-    return read_config(path_config_example())
-
-
 @pytest.fixture
-def database(config):
+def database():
+    database_config = read_config(database_config_template())
+    ramp_config = read_config(ramp_config_template())
     try:
-        create_test_db(config)
+        create_test_db(database_config, ramp_config)
         yield
     finally:
-        shutil.rmtree(config['ramp']['deployment_dir'], ignore_errors=True)
-        db, _ = setup_db(config['sqlalchemy'])
+        shutil.rmtree(
+            ramp_config['ramp']['deployment_dir'], ignore_errors=True
+        )
+        db, _ = setup_db(database_config['sqlalchemy'])
         Model.metadata.drop_all(db)
 
 
-def test_setup_db(database_config, database):
+def test_setup_db(database):
+    database_config = read_config(
+        database_config_template(), filter_section='sqlalchemy'
+    )
     db, Session = setup_db(database_config)
     with db.connect() as conn:
         session = Session(bind=conn)
@@ -43,7 +40,10 @@ def test_setup_db(database_config, database):
         assert len(file_type) > 0
 
 
-def test_session_scope(database_config, database):
+def test_session_scope(database):
+    database_config = read_config(
+        database_config_template(), filter_section='sqlalchemy'
+    )
     with session_scope(database_config) as session:
         file_type = session.query(SubmissionFileType).all()
         assert len(file_type) > 0
