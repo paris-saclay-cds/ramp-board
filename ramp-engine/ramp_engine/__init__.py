bc4b1d5a1d64e1834e51fbea677df7000d67846f
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2019-02-13 16:03:09 +0100

    add to available_workers

--- ramp-engine/ramp_engine/__init__.py
+++ ramp-engine/ramp_engine/__init__.py
@@ -1,11 +1,14 @@
 from .dispatcher import Dispatcher
 from .local import CondaEnvWorker
+from .aws import AWSWorker
 
 from ._version import __version__
 
-available_workers = {'conda': CondaEnvWorker}
+available_workers = {'conda': CondaEnvWorker,
+                     'aws': AWSWorker}
 
 __all__ = [
+    'AWSWorker',
     'CondaEnvWorker',
     'Dispatcher',
     'available_workers',
