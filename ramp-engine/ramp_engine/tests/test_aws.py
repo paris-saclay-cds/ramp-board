173c8266ab55c190854db00f68a015d78096ef85
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2019-02-14 14:03:19 +0100

    fix tests

--- ramp-engine/ramp_engine/tests/test_aws.py
+++ ramp-engine/ramp_engine/tests/test_aws.py
@@ -1,10 +1,10 @@
 """
 For testing the Amazon worker locally, you need:
-- the air_passengers starting kit with a 'local_starting_kit' submission
+- the iris starting kit with a 'starting_kit_local' submission
 - a config.yml file with credentials to interact with Amazon, and appropriate
-  fields for the air_passengers problem:
-    - ami_image_name: air_passengers_backend
-    - remote_ramp_kit_folder : /home/ubuntu/ramp-kits/air_passengers
+  fields for the iris problem:
+    - ami_image_name: ramp_aws_iris_test
+    - remote_ramp_kit_folder : /home/ubuntu/ramp-kits/iris
 
 """
 import logging
@@ -13,8 +13,8 @@ import shutil
 
 import pytest
 
-from rampbkd.aws import AWSWorker
-from rampbkd.config import read_backend_config
+from ramp_engine.aws import AWSWorker
+from ramp_utils import generate_worker_config, read_config
 
 
 HERE = os.path.dirname(__file__)
@@ -30,7 +30,7 @@ def test_aws_worker():
     if not os.path.isfile(os.path.join(HERE, 'config.yml')):
         pytest.skip("Only for local tests for now")
 
-    ramp_kit_dir = os.path.join(HERE, 'kits', 'air_passengers')
+    ramp_kit_dir = os.path.join(HERE, 'kits', 'iris')
     # if the prediction / log files are still there, remove them
     for subdir in os.listdir(os.path.join(ramp_kit_dir, 'predictions')):
         if os.path.isdir(subdir):
@@ -39,19 +39,20 @@ def test_aws_worker():
         if os.path.isdir(subdir):
             shutil.rmtree(subdir)
 
-    conf = read_backend_config(os.path.join(HERE, 'config.yml'))
-    worker = AWSWorker(conf['aws'], submission='local_starting_kit',
-                       ramp_kit_dir=ramp_kit_dir)
+    config = read_config(os.path.join(HERE, 'config.yml'))
+    worker_config = generate_worker_config(config)
+    worker = AWSWorker(worker_config, submission='starting_kit_local')
     worker.setup()
     assert worker.status == 'setup'
     worker.launch_submission()
-    assert worker.status == 'running'
+    assert worker.status in ('running', 'finished')
     worker.collect_results()
     assert worker.status == 'collected'
 
     assert os.path.isdir(os.path.join(
-        ramp_kit_dir, 'predictions', 'local_starting_kit', 'fold_0'))
+        ramp_kit_dir, 'predictions', 'starting_kit_local', 'fold_0'))
     assert os.path.isfile(os.path.join(
-        ramp_kit_dir, 'logs', 'local_starting_kit', 'log'))
+        ramp_kit_dir, 'logs', 'starting_kit_local', 'log'))
 
     worker.teardown()
+    assert worker.status == 'killed'
