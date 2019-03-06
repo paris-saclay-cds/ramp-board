0522fba267df02bde2f11f4b00ad0a7d80e84d0d
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2019-03-06 15:03:57 +0100

    Simplify flask config (default values)

--- ramp-utils/ramp_utils/frontend.py
+++ ramp-utils/ramp_utils/frontend.py
@@ -3,6 +3,14 @@ import six
 from .config_parser import read_config
 
 
+DEFAULT_CONFIG = {
+    'WTF_CSRF_ENABLED': True,
+    'LOG_FILENAME': 'None',
+    'MAX_CONTENT_LENGTH': 1073741824,
+    'SQLALCHEMY_TRACK_MODIFICATIONS': False,
+}
+
+
 def generate_flask_config(config):
     """Generate the configuration to deal with Flask.
 
@@ -19,8 +27,9 @@ def generate_flask_config(config):
     if isinstance(config, six.string_types):
         config = read_config(config, filter_section=['flask', 'sqlalchemy'])
 
-    flask_config = {key.upper(): value
-                    for key, value in config['flask'].items()}
+    user_flask_config = {
+        key.upper(): value for key, value in config['flask'].items()}
+    flask_config = DEFAULT_CONFIG.update(user_flask_config)
 
     database_config = config['sqlalchemy']
     flask_config['SQLALCHEMY_DATABASE_URI'] = \
