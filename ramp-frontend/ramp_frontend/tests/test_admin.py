fc99cc2fd7649d50f276f19134322217e192a405
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2019-03-13 23:34:49 +0100

    downgrade tornado

--- ramp-frontend/ramp_frontend/tests/test_admin.py
+++ ramp-frontend/ramp_frontend/tests/test_admin.py
@@ -29,7 +29,6 @@ from ramp_frontend.testing import login_scope
 def client_session():
     database_config = read_config(database_config_template())
     ramp_config = ramp_config_template()
-    print('XXXXXXXXXXXXXXXXXXXX')
     try:
         deployment_dir = create_toy_db(database_config, ramp_config)
         flask_config = generate_flask_config(database_config)
@@ -37,7 +36,6 @@ def client_session():
         app.config['TESTING'] = True
         app.config['WTF_CSRF_ENABLED'] = False
         with session_scope(database_config['sqlalchemy']) as session:
-            print('CREATE SESSION')
             yield app.test_client(), session
     finally:
         shutil.rmtree(deployment_dir, ignore_errors=True)
