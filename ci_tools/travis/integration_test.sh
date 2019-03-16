9555a5e745fe40716e55bd5b798127fb3d6052db
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2019-03-16 12:33:13 +0100

    TST: add an integration test

---
+++ ci_tools/travis/integration_test.sh
@@ -0,0 +1,35 @@
+cd $HOME
+mkdir ramp_deployment
+cd ramp_deployment
+ramp setup init
+echo "flask:
+    secret_key: abcdefghijkl
+    mail_server: smtp.gmail.com
+    mail_port: 587
+    mail_default_sender: ['RAMP admin', 'rampmailer@gmail.com']
+    mail_username: user
+    mail_password: password
+    mail_recipients: []
+    mail_use_tls: false
+    mail_use_ssl: true
+    mail_debug: false
+sqlalchemy:
+    drivername: postgresql
+    username: mrramp
+    password: mrramp
+    host: localhost
+    port: 5432
+    database: databoard_test" > config.yml
+ramp database add-user --login admin_user --password password --firstname firstname --lastname lastname --email admin@email.com --access-level admin
+ramp setup init-event --name iris_test
+echo "ramp:
+    problem_name: iris
+    event_name: iris_test
+    event_title: "Iris event"
+    event_is_public: true
+worker:
+    worker_type: conda
+    conda_env: ramp-iris" > events/iris_test/config.yml
+ramp setup deploy-event --event-config events/iris_test/config.yml
+ramp database add-submission --event iris_test --team admin_user --submission my_submission --path "$HOME/ramp_deployment/ramp-kits/iris/submissions/random_forest_10_10"
+ramp launch dispatcher --event-config events/iris/config.yml -vv
