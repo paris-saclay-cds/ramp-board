7294ca8c0c4782235e01ab65d7f07ac4535dfdf8
Maja <maja_ka@hotmail.com>
2019-11-08 17:23:40 +0100

    corrected conftest

--- ramp-frontend/ramp_frontend/tests/conftest.py
+++ ramp-frontend/ramp_frontend/tests/conftest.py
@@ -3,20 +3,23 @@ from sqlalchemy import create_engine, exc
 import contextlib
 
 @pytest.fixture(scope='session')
-def database():
+def connection(request):
     '''
     Create a Postgres database for the tests, and drop it when the tests are done.
     '''
     with contextlib.suppress(exc.ProgrammingError):
         engine = create_engine('postgresql:///postgres', isolation_level='AUTOCOMMIT')
+        #engine = create_engine('postgresql://mrramp:mrramp@localhost:5432')
         connection = engine.connect()
         try: 
             connection.execute("CREATE USER mrramp WITH PASSWORD 'mrramp';ALTER USER mrramp WITH SUPERUSER")
         except:
             print('mrramp already exists')
         connection.execute('CREATE DATABASE databoard_test OWNER mrramp')
-
+        connection.close()
+    
     @request.addfinalizer
     def drop_database():
-        connection.execute('DROP DATABASE data_test')
+        connection = engine.connect()
+        connection.execute('DROP DATABASE databoard_test')
         connection.close()
