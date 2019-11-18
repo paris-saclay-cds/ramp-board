7fadb74dc5e210abf254eabe1c2356dc748174f2
Maja <maja_ka@hotmail.com>
2019-11-18 12:57:48 +0100

    redefined error

--- ramp-frontend/ramp_frontend/tests/test_auth.py
+++ ramp-frontend/ramp_frontend/tests/test_auth.py
@@ -181,7 +181,7 @@ def test_sign_up(client_session):
     assert rv.status_code == 200
 
 
-@pytest.mark.skipif(raises=ConnectionRefusedError, reason="no possibility to \
+@pytest.mark.skipif(ConnectionRefusedError, reason="no possibility to \
                                                   set up the connection")
 def test_sign_up_with_approval(client_session):
     # check the sign-up and email confirmation framework
@@ -306,7 +306,7 @@ def test_update_profile(client_session):
         assert rv.status_code == 200
 
 
-@pytest.mark.skipif(raises=ConnectionRefusedError, reason="no possibility to \
+@pytest.mark.skipif(ConnectionRefusedError, reason="no possibility to \
                                                   set up the connection")
 def test_reset_password(client_session):
     client, session = client_session
@@ -373,7 +373,7 @@ def test_reset_password(client_session):
     assert check_password(new_password, user.hashed_password)
 
 
-@pytest.mark.skipif(raises=ConnectionRefusedError, reason="no possibility to \
+@pytest.mark.skipif(ConnectionRefusedError, reason="no possibility to \
                                                   set up the connection")
 def test_reset_token_error(client_session):
     client, session = client_session
