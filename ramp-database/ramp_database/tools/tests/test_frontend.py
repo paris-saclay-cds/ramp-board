6cc7a75cb9fe568bc076e9cc31cbf76e7b3012c0
Lucy Liu <jliu176@gmail.com>
2020-03-23 16:43:44 +0100

    test

--- ramp-database/ramp_database/tools/tests/test_frontend.py
+++ ramp-database/ramp_database/tools/tests/test_frontend.py
@@ -186,6 +186,8 @@ def test_is_accessible_leaderboard(session_toy_db):
     # simulate a user which authenticated and author of the submission to a
     # public event
     user.is_authenticated = True
+    a = user.is_active
+    b = is_user_signed_up(session_toy_db, event_name, usere)
     assert is_accessible_leaderboard(session_toy_db, event_name, user.name)
     # simulate an admin user
     user = get_user_by_name(session_toy_db, 'test_iris_admin')
