6ab5309e24c4ba8c5382e697892efe0481c8a9dd
maikia <maja_ka@hotmail.com>
2020-06-18 15:05:26 +0200

    updated the test_aws to take care of the needed empty directorys + the doc

--- ramp-engine/ramp_engine/tests/test_aws.py
+++ ramp-engine/ramp_engine/tests/test_aws.py
@@ -1,10 +1,7 @@
 """
-For testing the Amazon worker locally, you need:
-- the iris starting kit with a 'starting_kit_local' submission
-- a config.yml file with credentials to interact with Amazon, and appropriate
-  fields for the iris problem:
-    - ami_image_name: ramp_aws_iris_test
-    - remote_ramp_kit_folder : /home/ubuntu/ramp-kits/iris
+For testing the Amazon worker locally, you need to:
+copy the _config.yml file and rename it config.yml, then update it using the
+credentials to interact with Amazon
 
 """
 import logging
@@ -29,25 +26,23 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S')
-'''
-def test_simple():
-    import yaml
-    from ramp_engine.aws.worker import AWSWorker
-    fid = open("/home/maja/maja/ramp/ramp_code/ramp-board/ramp-engine/ramp_engine/tests/config.yml")
-    config = yaml.load(fid)
-    worker = AWSWorker(config['worker'], "random_forest_10_10")
-    worker.setup()
-    worker.launch_submission()
-    worker._is_submission_finished()
-    worker.status
-    worker.collect_results()
-    worker.teardown()
-'''
+
+
+def add_empty_dir(dir_name):
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
+
+
 def test_aws_worker():
     if not os.path.isfile(os.path.join(HERE, 'config.yml')):
         pytest.skip("Only for local tests for now")
 
     ramp_kit_dir = os.path.join(HERE, 'kits', 'iris')
+
+    # make sure predictio and log dirs exist, if not, add them
+    add_empty_dir(os.path.join(ramp_kit_dir, 'predictions'))
+    add_empty_dir(os.path.join(ramp_kit_dir, 'logs'))
+
     # if the prediction / log files are still there, remove them
     for subdir in os.listdir(os.path.join(ramp_kit_dir, 'predictions')):
         if os.path.isdir(subdir):
