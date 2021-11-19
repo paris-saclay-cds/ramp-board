a5d5a82ba6eb6336601dcac528ee63d4e0273d2b
Roman Yurchak <rth.yurchak@gmail.com>
2021-11-19 08:56:51 +0100

    CI Fix CircleCI

--- ci_tools/circle/build_doc.sh
+++ ci_tools/circle/build_doc.sh
@@ -8,6 +8,7 @@ wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
 chmod +x miniconda.sh && ./miniconda.sh -b -p $MINICONDA_PATH
 export PATH="$MINICONDA_PATH/bin:$PATH"
 conda update --yes --quiet conda
+conda config --add channels conda-forge
 
 # create the environment
 conda create --yes -n testenv python=3.8
